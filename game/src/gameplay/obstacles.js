d0a81799ebf7d65970b76cb5f7fe9f9eca319d69
Ju Jeong Min <anjshdkdl99@gmail.com>
2026-07-21 15:28:38 +0900

    feat: add obstacles.js module for static cover obstacles

---
+++ game/src/gameplay/obstacles.js
@@ -0,0 +1,45 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+const CRATE_URL = '/models/crate.glb';
+const SACK_TRENCH_URL = '/models/sack-trench.glb';
+const CRATE_SCALE = 6;
+const SACK_TRENCH_SCALE = 1;
+const GROUND_Y = -1.0;
+
+const PLACEMENTS = [
+  { type: 'sackTrench', x: -3, z: -9 },
+  { type: 'sackTrench', x: 2.5, z: -10 },
+  { type: 'crate', x: 0.5, z: -7.5 },
+  { type: 'crate', x: -1.5, z: -8.5 },
+];
+
+export function loadObstacles(scene) {
+  const loader = new GLTFLoader();
+  return Promise.all([loader.loadAsync(CRATE_URL), loader.loadAsync(SACK_TRENCH_URL)]).then(
+    ([crateGltf, sackTrenchGltf]) => {
+      const blockingMeshes = [];
+
+      for (const placement of PLACEMENTS) {
+        const isCrate = placement.type === 'crate';
+        const template = isCrate ? crateGltf.scene : sackTrenchGltf.scene;
+        const scale = isCrate ? CRATE_SCALE : SACK_TRENCH_SCALE;
+
+        const instance = template.clone();
+        instance.scale.setScalar(scale);
+        instance.position.set(placement.x, GROUND_Y, placement.z);
+        scene.add(instance);
+
+        instance.traverse((object) => {
+          if (object.isMesh) blockingMeshes.push(object);
+        });
+      }
+
+      return {
+        getBlockingMeshes() {
+          return blockingMeshes;
+        },
+      };
+    }
+  );
+}
